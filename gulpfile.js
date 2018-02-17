de7a3e86522370cd506b423d228ad658c2f9bc86
Florian Rival <Florian.Rival@gmail.com>
2018-02-17 19:35:50 +0100

    Have each language in its own directory

--- gulpfile.js
+++ gulpfile.js
@@ -18,136 +18,166 @@ gulp.task('default', ['sass', 'uglify', 'wiredep', 'imagemin']);
 /**
  * Build styles files
  */
-gulp.task('sass', function () {
-    gulp.src('src/styles/main.scss')
-        .pipe(sass())
-  		.pipe(rename("styles.css"))
-        .pipe(gulp.dest('public/styles/'));
+gulp.task('sass', function() {
+  gulp
+    .src('src/styles/main.scss')
+    .pipe(sass())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('public/styles/'));
 });
 
 /**
  * Build minified javascript files
  */
-gulp.task('uglify', function () {
-    gulp.src(['src/js/*.js'])
-        .pipe(uglify())
-  		.pipe(rename("scripts.min.js"))
-        .pipe(gulp.dest('public/js'));
+gulp.task('uglify', function() {
+  gulp
+    .src(['src/js/*.js'])
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('public/js'));
 });
 
 /**
  * Add bower dependencies into .html files
  */
-gulp.task('wiredep', ['ejs'], function () {
-    gulp.src(['public/*.html'])
-        .pipe(wiredep())
-        .pipe(gulp.dest('public'));
+gulp.task('wiredep', ['ejs'], function() {
+  gulp
+    .src(['public/*.html'])
+    .pipe(wiredep())
+    .pipe(gulp.dest('public'));
 });
 
 /**
  * Create HTML files from .ejs files
  */
-gulp.task('ejs', function (cb) {
-	var langs = getAvailableLanguagesCatalogs();
-	gutil.log("Found these languages catalogs (with translation ratio >0.6): ", langs);
+gulp.task('ejs', function(cb) {
+  var langs = getAvailableLanguagesCatalogs();
+  gutil.log(
+    'Found these languages catalogs (with translation ratio >0.6): ',
+    langs
+  );
 
-	//A separate gulp stream must be launched for generating html files
-	//for each language.
-	allTasks = langs.map(function(langCode) {
-		return function(cb) {
-			//Open the language catalog
-    		var catalog = langCode == "en" ? "" : require('./locale/' + langCode + '.json');
+  //A separate gulp stream must be launched for generating html files
+  //for each language.
+  allTasks = langs.map(function(langCode) {
+    return function(cb) {
+      //Open the language catalog
+      var isDefaultLanguage = langCode == 'en';
+      var catalog = isDefaultLanguage
+        ? ''
+        : require('./locale/' + langCode + '.json');
+      var root = isDefaultLanguage ? '.' : '..';
+      var destination = 'public/' + (isDefaultLanguage ? '' : langCode);
 
-    		//Launch a stream for generating the .ejs file for this language
-			gulp.src('src/*.ejs')
-			.on('end', function() {
-				gutil.log("Ended generating " + langCode + " files from .ejs sources");
-				cb();
-			})
-		    .pipe(ejs({
-		    	//The EJS helper function to be used to wrap any string to be translated
-		    	_: function(str) {
-		    		if (str == "__langCode__") {
-		    			return langCode;
-		    		} else if (catalog.hasOwnProperty(str)) {
-			    		return catalog[str];
-			    	}
+      //Launch a stream for generating the .ejs file for this language
+      gulp
+        .src('src/*.ejs')
+        .on('end', function() {
+          gutil.log(
+            'Ended generating ' + langCode + ' files from .ejs sources'
+          );
+          cb();
+        })
+        .pipe(
+          ejs({
+            //The EJS helper function to be used to wrap any string to be translated
+            _: function(str) {
+              if (str == '__langCode__') {
+                return langCode;
+              } else if (catalog.hasOwnProperty(str)) {
+                return catalog[str];
+              }
 
-			    	return str;
-			    }
-			}).on('error', cb))
-			.pipe(rename(function(path) {
-				if (langCode !== "en") {
-					path.basename += "-" + langCode;
-				}
-			}))
-		    .pipe(gulp.dest('public'));
-		}
-	});
+              return str;
+            },
+            getAssetsRoot: function() {
+              return root + '/assets';
+            },
+            getStylesRoot: function() {
+              return root + '/styles';
+            },
+            getJsRoot: function() {
+              return root + '/js';
+            },
+            getBowerComponentsRoot: function() {
+              return root + '/bower_components';
+            },
+          }).on('error', cb)
+        )
+        .pipe(gulp.dest(destination));
+    };
+  });
 
-	async.parallel(allTasks, function(err) {
-		cb(err);
-	})
+  async.parallel(allTasks, function(err) {
+    cb(err);
+  });
 });
 
 /**
  * Create catalog.json
  */
-gulp.task('update-translation', function () {
-	var allStrings = {};
+gulp.task('update-translation', function() {
+  var allStrings = {};
 
-	gulp.src('src/*.ejs')
-		.on('end', function() {
-			fs.writeFile('locale/catalog.json', JSON.stringify(allStrings, null, 4), function(err) {
-			    if(err) {
-					gutil.log(err);
-					return;
-			    }
+  gulp
+    .src('src/*.ejs')
+    .on('end', function() {
+      fs.writeFile(
+        'locale/catalog.json',
+        JSON.stringify(allStrings, null, 4),
+        function(err) {
+          if (err) {
+            gutil.log(err);
+            return;
+          }
 
-			    gutil.log("Catalog with all strings saved to locale/catalog.json");
-			});
-		})
-	    .pipe(ejs({
-	    	"_": function(str) {
-	    		if (str !== "__langCode__")
-		    		allStrings[str] = str;
-		    }
-		}).on('error', gutil.log));
+          gutil.log('Catalog with all strings saved to locale/catalog.json');
+        }
+      );
+    })
+    .pipe(
+      ejs({
+        _: function(str) {
+          if (str !== '__langCode__') allStrings[str] = str;
+        },
+      }).on('error', gutil.log)
+    );
 });
 
 /**
  * Watch changes in the src directory and launch the appropriate tasks.
  */
-gulp.task('watch', function () {
-    gulp.watch('src/styles/**/*.scss', ['sass']);
-    gulp.watch('src/js/*.js', ['uglify']);
-    gulp.watch('src/assets/**/*.svg', ['copy-svg']);
-    gulp.watch(['src/*.ejs', 'locale/*.json'], ['wiredep']);
+gulp.task('watch', function() {
+  gulp.watch('src/styles/**/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['uglify']);
+  gulp.watch('src/assets/**/*.svg', ['copy-svg']);
+  gulp.watch(['src/*.ejs', 'locale/*.json'], ['wiredep']);
 });
 
 /**
  * Minimize assets size
  */
-gulp.task('imagemin', function () {
-	//Do not minimize this file, quality is too low
-	gulp.src('src/assets/jumbotron-bg.png')
-	    .pipe(gulp.dest('public/assets'));
+gulp.task('imagemin', function() {
+  //Do not minimize this file, quality is too low
+  gulp.src('src/assets/jumbotron-bg.png').pipe(gulp.dest('public/assets'));
 
-    return gulp.src(['src/assets/**/*', '!src/assets/jumbotron-bg.png'])
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('public/assets'));
+  return gulp
+    .src(['src/assets/**/*', '!src/assets/jumbotron-bg.png'])
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [pngquant()],
+      })
+    )
+    .pipe(gulp.dest('public/assets'));
 });
 
 /**
  * Copy svg assets files
  */
 gulp.task('copy-svg', function() {
-	return gulp.src('src/assets/**/*.svg')
-	    .pipe(gulp.dest('public/assets'));
+  return gulp.src('src/assets/**/*.svg').pipe(gulp.dest('public/assets'));
 });
 
 /**
@@ -155,18 +185,20 @@ gulp.task('copy-svg', function() {
  * in locale directory, with a translation ratio that is high enough.
  */
 function getAvailableLanguagesCatalogs() {
-	var availableLangs = ["en"];
+  var availableLangs = ['en'];
 
-	fs.readdirSync('locale').forEach(function(file) {
-		if (path.basename(file) === "catalog.json" ||
-			path.extname(file) !== ".json")
-			return;
+  fs.readdirSync('locale').forEach(function(file) {
+    if (
+      path.basename(file) === 'catalog.json' ||
+      path.extname(file) !== '.json'
+    )
+      return;
 
-		var ratio = getTranslatedRatio(require('./locale/' + file));
-		if (ratio > 0.6) availableLangs.push(path.basename(file, ".json"));
-	});
+    var ratio = getTranslatedRatio(require('./locale/' + file));
+    if (ratio > 0.6) availableLangs.push(path.basename(file, '.json'));
+  });
 
-	return availableLangs;
+  return availableLangs;
 }
 
 /**
@@ -174,16 +206,16 @@ function getAvailableLanguagesCatalogs() {
  * in the specified object.
  */
 function getTranslatedRatio(catalog) {
-	var translatedCount = 0;
-	var totalCount = 0;
-	for (var p in catalog) {
-		if (catalog.hasOwnProperty(p)) {
-			if (p !== catalog[p]) {
-				translatedCount++;
-			}
-			totalCount++;
-		}
-	}
+  var translatedCount = 0;
+  var totalCount = 0;
+  for (var p in catalog) {
+    if (catalog.hasOwnProperty(p)) {
+      if (p !== catalog[p]) {
+        translatedCount++;
+      }
+      totalCount++;
+    }
+  }
 
-	return translatedCount/totalCount;
+  return translatedCount / totalCount;
 }
