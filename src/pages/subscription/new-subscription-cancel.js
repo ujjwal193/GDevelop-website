8bb1e983f7082e0cb6668dea1e2cea50bc05061e
Florian Rival <Florian.Rival@gmail.com>
2019-09-06 08:25:12 +0100

    Add pages for users getting a subscription for GDevelop

---
+++ src/pages/subscription/new-subscription-cancel.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import PageContainer from '../../lib/PageContainer';
+import BigTitle from '../../components/BigTitle';
+import Paragraph from '../../components/Paragraph';
+import TransparentContainer from '../../components/Containers/TransparentContainer';
+import Spacer from '../../components/Grid/Spacer';
+import MarkdownText from '../../components/MarkdownText';
+
+const NotFoundPage = props => (
+  <PageContainer {...props.pathContext}>
+    {t => (
+      <React.Fragment>
+        <Helmet title="GDevelop - Page not found">
+          <html lang="en" />
+        </Helmet>
+        <Navbar t={t} noTransparency />
+        <Spacer height="200px" />
+        <TransparentContainer>
+          <BigTitle>Your subscription is not activated</BigTitle>
+          <Paragraph>
+            <MarkdownText
+              source={t(
+                'There either has been an error or you cancelled the subscription. If you want to get a subscription to support GDevelop and unlock online services, try again to choose one from GDevelop (*My Profile* menu).'
+              )}
+            />
+          </Paragraph>
+          <Paragraph>
+            <MarkdownText
+              source={t(
+                'As an open-source software, GDevelop requires time and money to be developed and sustainable. Thanks for your contribution! If you want to get more involved, you can also [read all the ways](/contribute) to contribute to GDevelop ecosystem.'
+              )}
+            />
+          </Paragraph>
+        </TransparentContainer>
+        <Spacer height="200px" />
+        <Footer t={t} />
+      </React.Fragment>
+    )}
+  </PageContainer>
+);
+
+export default NotFoundPage;
---
+++ src/pages/subscription/new-subscription-success.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import PageContainer from '../../lib/PageContainer';
+import BigTitle from '../../components/BigTitle';
+import Paragraph from '../../components/Paragraph';
+import TransparentContainer from '../../components/Containers/TransparentContainer';
+import Spacer from '../../components/Grid/Spacer';
+import MarkdownText from '../../components/MarkdownText';
+
+const NotFoundPage = props => (
+  <PageContainer {...props.pathContext}>
+    {t => (
+      <React.Fragment>
+        <Helmet title="GDevelop - Page not found">
+          <html lang="en" />
+        </Helmet>
+        <Navbar t={t} noTransparency />
+        <Spacer height="200px" />
+        <TransparentContainer>
+          <BigTitle>
+            Thanks for getting a subscription and supporting GDevelop!
+          </BigTitle>
+          <Paragraph>
+            <MarkdownText
+              source={t(
+                'You can now **go back to GDevelop** to use the additional online exports and services.'
+              )}
+            />
+          </Paragraph>
+          <Paragraph>
+            <MarkdownText
+              source={t(
+                'As an open-source software, GDevelop requires time and money to be developed and sustainable. Thanks for your contribution! If you want to get more involved, you can also [read all the ways](/contribute) to contribute to GDevelop ecosystem.'
+              )}
+            />
+          </Paragraph>
+        </TransparentContainer>
+        <Spacer height="200px" />
+        <Footer t={t} />
+      </React.Fragment>
+    )}
+  </PageContainer>
+);
+
+export default NotFoundPage;
