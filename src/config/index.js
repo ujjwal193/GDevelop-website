d66e5b847eed79446670f6a8c0a844be04a1d166
Florian Rival <Florian.Rival@gmail.com>
2020-06-09 20:08:12 +0100

    Update download links

--- src/config/index.js
+++ src/config/index.js
@@ -14,9 +14,9 @@ export default {
   onlineEditorUrl: 'https://editor.gdevelop-app.com',
 
   gdevelopWindowsUrl:
-    'https://github.com/4ian/GDevelop/releases/download/v5.0.0-beta93/GDevelop-5-Setup-5.0.0-beta93.exe',
+    'https://github.com/4ian/GDevelop/releases/download/v5.0.0-beta95/GDevelop-5-Setup-5.0.0-beta95.exe',
   gdevelopMacOSUrl:
-    'https://github.com/4ian/GDevelop/releases/download/v5.0.0-beta93/GDevelop-5-5.0.0-beta93.dmg',
+    'https://github.com/4ian/GDevelop/releases/download/v5.0.0-beta95/GDevelop-5-5.0.0-beta95.dmg',
   gdevelopLinuxUrl:
-    'https://github.com/4ian/GDevelop/releases/download/v5.0.0-beta93/GDevelop-5-5.0.0-beta93.AppImage',
+    'https://github.com/4ian/GDevelop/releases/download/v5.0.0-beta95/GDevelop-5-5.0.0-beta95.AppImage',
 };
